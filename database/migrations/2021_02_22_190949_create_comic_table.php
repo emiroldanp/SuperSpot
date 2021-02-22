30028bbb12d8546c09ee848b876082e8a8110787
Alfredo Osuna Torres <38539148+aosunt25@users.noreply.github.com>
2021-02-22 15:00:42 -0600

    Creacion de migrations
    Si hicieron todos los documentos de las Migration para la base de datos

---
+++ database/migrations/2021_02_22_190949_create_comic_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comic', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->double("rating");
+            $table->integer("volume");
+            $table->foreignId("series_id")->references("id")->on("series");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comic');
+    }
+}

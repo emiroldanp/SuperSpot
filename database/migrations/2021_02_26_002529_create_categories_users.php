761bf4f0bb77bbc03261b17f1e13f6cfbb010feb
Alfredo Osuna <alfredoosuna40@gmail.com>
2021-02-25 18:26:33 -0600

    Cambio de base de datos

--- database/migrations/2021_02_26_002529_create_categories_users.php
+++ database/migrations/2021_02_26_002529_create_categories_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreateCategoriesUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('categories_users', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string("content");
-            $table->foreignId('serie_id')->references('id')->on('series');
-            //$table->foreignId('user_id');
-
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('category_id')->references('id')->on('categories');
         });
     }
 
@@ -30,6 +28,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('categories_users');
     }
 }
